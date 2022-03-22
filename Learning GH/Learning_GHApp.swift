478c1344762719a90851dc19c982b3288cd46c33
DJ <denrichlumpkin89@gmail.com>
2022-03-22 18:58:11 +0100

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Denrich Lumpkin on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
